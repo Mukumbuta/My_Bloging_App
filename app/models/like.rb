6663155827ce8f0e226fecc697dd53e8dee48f13
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-03-11 15:48:04 +0200

    defined like update counter  in a private method for the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  post.update(likes_counter: post.likes_counter + 1)
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
