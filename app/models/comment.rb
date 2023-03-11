5efd57598cf69d30bb65232c6eb7c1512d4121c6
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-03-11 15:04:59 +0200

    Add models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  post.update(comments_counter: post.comments_counter + 1)
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  post.update(likes_counter: post.likes_counter + 1)
 end
