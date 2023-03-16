3f16ca41928b0c25b44af3fcda0a11539b343fd8
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-03-16 20:46:42 +0200

    add controllers

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,6 @@ class Comment < ApplicationRecord
   private
 
   def update_comments_count
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 end
