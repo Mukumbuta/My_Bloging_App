c59cf46b1ad4f8cace8184fd0fd0e1ee5fe5765f
Emmanuel Simasiku <mukumbutasimasiku@gmail.com>
2023-05-13 19:15:07 +0200

    Creating post fo the logged in user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,20 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
-  end
-
   def create
     @comment = Comment.new(comment_params)
-    @comment.user_id = params[:user_id]
+    @comment.user = current_user
     @comment.post_id = params[:post_id]
     @post = Post.find(params[:post_id])
 
     if @comment.save
-      redirect_to user_posts_path(@post.user_id)
+      redirect_to @post, notice: 'Comment was successfully created.'
     else
-      render :new, alert: 'ERROR! something went wrong while creating the comment'
+      render :new
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:body)
   end
 end
